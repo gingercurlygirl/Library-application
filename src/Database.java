766b3b7bf06f5e6aba862ee8fd14c745d6b3fb02
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-02-19 16:28:06 +0100

    add connector, create database with url and password

---
+++ src/Database.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String url = "jdbc:mysql://localhost:3306/library";
+
+    private static final String user = System.getenv("MYSQL_USER");
+
+    private static final String password = System.getenv("MYSQL_PASSWORD");
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conn;
+    }
+}
